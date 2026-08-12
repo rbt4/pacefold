ec67051076d6736c58f97ae728ccdd4d22be871b
rbt4 <a.m.parekh@gmail.com>
2026-08-12 16:03:00 -0400

    Pacefold 27: refresh offline shell without resetting durable cue state

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
-const VERSION='26.0.0';
-const CACHE_NAME=`pacefold-v${VERSION}-r1`;
+const VERSION='27.0.0';
+const CACHE_NAME=`pacefold-v${VERSION}-polish-r1`;
 const ROOT=new URL('./',self.location.href);
 const path=value=>new URL(value,ROOT).href;
 const SHELL=[
